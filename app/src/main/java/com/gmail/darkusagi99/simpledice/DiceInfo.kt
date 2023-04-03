57b3a7f510f9fd10eddd7e8e639ef455f406eba1
darkusagi99 <darkusagi99@gmail.com>
2023-04-03 21:04:57 +0200

    Gestion de la forme des dés - Suite - Triangle et code

--- app/src/main/java/com/gmail/darkusagi99/simpledice/DiceInfo.kt
+++ app/src/main/java/com/gmail/darkusagi99/simpledice/DiceInfo.kt
@@ -14,6 +14,15 @@ object DiceInfo {
      * */
     var SETTING_MODE : Int = 0
 
+    /**
+     * List of number of face by shapes
+     * */
+    var TRIANGLE_LIST : List<Int> =  listOf(4, 8, 20, 23, 60)
+    var PENTAGON_LIST : List<Int> =  listOf(10, 17, 19, 22, 26, 28)
+    var DIAMOND_LIST : List<Int> =  listOf(5, 12, 24, 27, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59)
+    var KITE_LIST : List<Int> =  listOf(7, 25, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47)
+    var HEXAGON_LIST : List<Int> =  listOf(13, 14, 15)
+
     fun addDice(item: DiceItem): Int {
         ITEMS.add(item)
         return ITEMS.size
@@ -25,11 +34,32 @@ object DiceInfo {
 
     fun createNewDice(maxValue : Int): DiceItem {
 
-        var vShape : Int = R.drawable.ic_dice_square
+        val vShape: Int
 
-        if (maxValue >= 100) {
-            vShape = R.drawable.ic_dice_circle
+        when {
 
+            // 2 -> Cercle
+            (maxValue == 2) -> vShape = R.drawable.ic_dice_circle
+
+            // 4, 8 et 20, 23, 60 -> Triangle
+            (TRIANGLE_LIST.contains(maxValue)) -> vShape = R.drawable.ic_dice_triangle
+
+            // 10, 17, 19, 22, 26, 28 -> Pentagone
+            (PENTAGON_LIST.contains(maxValue)) -> vShape = R.drawable.ic_dice_triangle
+
+            // 5, 12, 24, 27 48 à 60 -> Diamant
+            (DIAMOND_LIST.contains(maxValue)) -> vShape = R.drawable.ic_dice_triangle
+
+            // 7, 25, 30 à 47 -> Losange
+            (KITE_LIST.contains(maxValue)) -> vShape = R.drawable.ic_dice_triangle
+
+            // 13, 14, 15 -> Hexagone
+            (HEXAGON_LIST.contains(maxValue)) -> vShape = R.drawable.ic_dice_triangle
+
+            // Nbe faces > 60 -> Cercle
+            (maxValue > 60) -> vShape = R.drawable.ic_dice_circle
+            // Sinon -> Carré (3, 6, 9, 16, 18, 21)
+            else -> vShape = R.drawable.ic_dice_square
         }
 
         return DiceItem(1, maxValue + 1, 0, vShape)
@@ -55,5 +85,5 @@ object DiceInfo {
     data class DiceItem(var min: Int, var max: Int, var lastRoll: Int, var shape: Int) {
         override fun toString(): String = "last roll : $lastRoll"
     }
-    
+
 }
